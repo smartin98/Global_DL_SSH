699c8316362bc3867472114256692d857dba0f37
smartin98 <s98amartin@gmail.com>
2024-01-15 15:32:34 -0800

    update readme with links, add subset_for_flux.py

--- .ipynb_checkpoints/README-checkpoint.md
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -1,21 +1,23 @@
 # Global_DL_SSH
 Neural network method for mapping global sea surface height (SSH) from satellite altimetry and sea surface temperature (SST) observations.
 
-This repo contains python code to fully reproduce our training and inference workflow for global SSH mapping from satellite altimetry and gridded SST observations (ADD PREPRINT LINK). 
+This repo contains python code to fully reproduce our training and inference workflow for global SSH mapping from satellite altimetry and gridded SST observations ([pre-print](https://doi.org/10.31223/X5W676)). 
 
 Disclaimer: this workflow was developed with research/method development in mind, not to be a user-friendly package for production. In future, we hope to refactor the code to streamline the pipeline from observations to maps, make the code more user friendly, and make the worflow cloud-native to avoid having to download large datasets. That said, the code in this repo plus publicly available datasets (linked throughout the repo) is sufficient to fully reproduce our workflow given enough storage space ~15TB and access to multiple CPUs and GPUs.
 
 Steps to reproduce SSH mapping workflow:
 
-1. Download L4 MUR SST product for desired years (2010-2022 in our paper) from NASA PO.DAAC, L3 SSH observations from CMEMS, MDT from CMEMS, bathymetry from GEBCO, and auxilliary data sets from the 2023a Ocean Data Challenge (./data/sad).
+1. Download L4 MUR SST product for desired years (2010-2022 in our paper) from NASA PO.DAAC, L3 SSH observations from CMEMS, MDT from CMEMS, bathymetry from GEBCO, and auxilliary data sets from the 2023a [Ocean Data Challenge](https://github.com/ocean-data-challenges/2023a_SSH_mapping_OSE) (./data/sad).
 2. Create a 'raw' directory with 5615 subdirectories named with integers from 0 to 5614 inclusive.
 3. Run generate_global_data.py script. This is the slowest part of the pipeline and could take multiple days depending on number of CPUs available, this is a one time pre-processing step though to subset the observations in the local patches and doesn't need to be repeated if detals of the training/inference procedure is changed at a later date.
 4. Run pre_process_training.py to generate ML-ready TFRecord input-output pairs for both training and cross-validation.
 5. Run simvp_ddp_training.py to train the neural network (SimVP).
 6. Run pre_process_testing.py to generate the input data for creating global SSH maps for 2019, the withheld testing year. NB the input currently withholds Saral/Altika for independent evaluation purposes, the desired altimeter constellation can be specified in the code.
-7. Run simvp_predict_ssh.py to predict SSH on all the local patches for 2019
+7. Run simvp_predict_ssh.py to predict SSH on all the local patches for 2019.
 8. Run merge_maps.py to merge the local SSH patch reconstructions and save to NetCDF.
+9. Optionally, run calculate_currents.py to calculate surface geostrophic currents, vorticity, and strain rate from the SSH maps.
+10. Optionally, run subset_for_flux.py to subset the data for the local coarse graining analysis used in the paper to study KE cascade (coarse graining can then be performed using [FlowSieve](https://github.com/husseinaluie/FlowSieve)).
 
-We also provide checkpoint files for the models used in the paper, in which case steps 4-5 can be skipped. These checkpoint files were too large for GitHub but are stored in a Harvard DataVerse along with the SSH maps (ADD REPO LINK ONCE UPLOADED).
+We also provide checkpoint files for the models used in the paper, in which case steps 4-5 can be skipped. These checkpoint files were too large for GitHub but are stored in a Harvard DataVerse [repo](https://doi.org/10.7910/DVN/H4HQGD) along with the SSH maps.
 
 
---
+++ .ipynb_checkpoints/subset_for_flux-checkpoint.py
@@ -0,0 +1,152 @@
+import numpy as np
+import xarray as xr
+from glob import glob
+from pyproj import Proj
+from scipy import interpolate
+import pandas as pd
+
+mdt_path = 'path to netcdf containing MDT velocities (download from dataverse)'
+simvp_path = 'path to directory containing SimVP maps (download from dataverse)'
+duacs_path = 'path to directory containing DUACS maps (download from https://github.com/ocean-data-challenges/2023a_SSH_mapping_OSE)'
+ice_path = 'path to NetCDF containing sea ice mask for 2019 (download from dataverse)'
+save_dir = 'path to directory to save coarse graining subsets'
+
+
+ds_mdt = xr.open_dataset(mdt_path)
+
+
+time_min = '2019-01-01'
+time_max = '2019-12-31'
+list_of_files = sorted(glob(simvp_path + '/*.nc'))
+ds = xr.open_mfdataset(list_of_files, combine='by_coords')
+ds = ds.sel(time=slice(time_min,time_max), drop = True)
+ds = ds.sel(latitude=slice(-70,80),drop=True)
+
+
+list_of_files = sorted(glob(duacs_path+'/*.nc'))
+ds_duacs = xr.open_mfdataset(list_of_files, combine='by_coords')
+ds_duacs = ds_duacs.sel(latitude=slice(-70,80),drop=True)
+ds_duacs['longitude'] = ds_duacs['longitude'].assign_attrs({'units':'degrees_east','_CoordinateAxisType':'Lon'})
+ds_ice = xr.open_dataset(ice_path)
+ds_ice = ds_ice.sel(latitude=slice(-70,80),drop=True)
+ds_ice = ds_ice.reindex(time=ds_duacs['time'], method='nearest')
+ds_duacs = ds_duacs.where(ds_ice['ice_conc']<1) # Make sea ice mask same as used for SimVP
+
+
+def subset(ds, ds_mdt, lon0, lat0, domain_name, duacs):
+    print(domain_name)
+    padding = 20
+    lon_min = lon0 - padding
+    lon_max = lon0 + padding
+    lat_min = lat0 - padding
+    lat_max = lat0 + padding
+    
+    ds_crop = ds.sel(longitude=slice(lon_min,lon_max),drop=True).sel(latitude=slice(lat_min,lat_max),drop=True)
+    ds_mdt_crop = ds_mdt.sel(longitude=slice(lon_min,lon_max),drop=True).sel(latitude=slice(lat_min,lat_max),drop=True)
+    
+    lon = ds_crop['longitude']
+    lat = ds_crop['latitude']
+    lon,lat = np.meshgrid(lon, lat)
+    lon = lon.flatten()
+    lat = lat.flatten()
+    proj = Proj(proj='laea', lat_0=lat0, lon_0=lon0, units='m', datum='WGS84')
+    x, y = proj(lon, lat)
+    ugosa = np.array(ds_crop['ugosa'].load())
+    vgosa = np.array(ds_crop['vgosa'].load())
+    if not duacs:
+        ugos = ugosa + np.array(ds_mdt_crop['u_mdt'])
+        vgos = vgosa + np.array(ds_mdt_crop['v_mdt'])
+    else:
+        ugos = np.array(ds_crop['ugos'].load())
+        vgos = np.array(ds_crop['vgos'].load())
+
+    if duacs:
+        ugos = np.moveaxis(ugos, [0,1,2], [2,0,1])
+        vgos = np.moveaxis(vgos, [0,1,2], [2,0,1])
+    
+    x_grid = np.arange(-128*10*1e3,128*10*1e3,10*1e3).astype('float')
+    y_grid = np.arange(-128*10*1e3,128*10*1e3,10*1e3).astype('float')
+    x_grid,y_grid = np.meshgrid(x_grid,y_grid)
+    x_grid = x_grid.flatten()
+    y_grid = y_grid.flatten()
+
+    ugos_interp = np.zeros((256,256,365))
+    vgos_interp = np.zeros((256,256,365))
+    
+    for t in range(365):
+        if t%10==0:
+            print(t)
+        arrs = [ugos[:,:,t],vgos[:,:,t]]
+        for i, arr in enumerate(arrs):
+            x_loop = x.copy()
+            y_loop = y.copy()
+            var = arr.flatten()
+            var = var[x_loop>-1280e3-30e3]
+            y_loop = y_loop[x_loop>-1280e3-30e3]
+            x_loop = x_loop[x_loop>-1280e3-30e3]
+
+            var = var[x_loop<1280e3+30e3]
+            y_loop = y_loop[x_loop<1280e3+30e3]
+            x_loop = x_loop[x_loop<1280e3+30e3]
+
+            var = var[y_loop<1280e3+30e3]
+            x_loop = x_loop[y_loop<1280e3+30e3]
+            y_loop = y_loop[y_loop<1280e3+30e3]
+
+            var = var[y_loop>-1280e3-30e3]
+            x_loop = x_loop[y_loop>-1280e3-30e3]
+            y_loop = y_loop[y_loop>-1280e3-30e3]
+
+            interpolator = interpolate.LinearNDInterpolator(np.stack((x_loop,y_loop),axis=-1),var)
+
+            var_interp = interpolator(x_grid, y_grid)
+            var_interp = var_interp.reshape(256,256)
+
+            if i==0:
+                ugos_interp[:,:,t] = var_interp
+            elif i==1:
+                vgos_interp[:,:,t] = var_interp
+                
+    ugos_interp[np.isnan(ugos_interp)] = 0
+    vgos_interp[np.isnan(vgos_interp)] = 0
+        
+    start_date = pd.Timestamp("2019-01-01")
+    end_date = pd.Timestamp("2019-12-31")
+    time = pd.date_range(start=start_date, end=end_date)
+    
+    da_ugos = xr.DataArray(np.expand_dims(np.moveaxis(ugos_interp, [0,1,2], [1,2,0]),axis=1), dims=("time","depth","latitude", "longitude"), coords={"time": time, "depth":np.zeros(1),"latitude": np.arange(-128*10*1e3,128*10*1e3,10*1e3).astype('float'), "longitude": np.arange(-128*10*1e3,128*10*1e3,10*1e3).astype('float')})
+    da_vgos = xr.DataArray(np.expand_dims(np.moveaxis(vgos_interp, [0,1,2], [1,2,0]),axis=1), dims=("time","depth","latitude", "longitude"), coords={"time": time, "depth":np.zeros(1),"latitude": np.arange(-128*10*1e3,128*10*1e3,10*1e3).astype('float'), "longitude": np.arange(-128*10*1e3,128*10*1e3,10*1e3).astype('float')})
+    
+    ds_gos = xr.Dataset({"uo":da_ugos,"vo":da_vgos})
+    
+    if duacs:
+        ds_gos.to_netcdf(save_dir+domain_name+f'_{lon0}_{lat0}.nc')
+    else:
+        ds_gos.to_netcdf(save_dir+domain_name+f'_{lon0}_{lat0}.nc')
+    
+
+## N ATLANTIC
+lon0 = 330
+lat0 = 45
+subset(ds ,ds_mdt , lon0, lat0, domain_name='natlantic', duacs=False)
+subset(ds_duacs, ds_mdt, lon0, lat0, domain_name='natlantic', duacs=True)
+
+## STCC
+lon0 = 155
+lat0 = 25
+subset(ds_duacs, ds_mdt, lon0, lat0, domain_name='stcc', duacs=True)
+subset(ds ,ds_mdt , lon0, lat0, domain_name='stcc', duacs=False)
+
+## S PACIFIC
+
+lon0 = 200
+lat0 = -25
+subset(ds ,ds_mdt , lon0, lat0, domain_name='spacific', duacs=False)
+subset(ds_duacs, ds_mdt, lon0, lat0, domain_name='spacific', duacs=True)
+
+## KUROSHIO
+
+lon0 = 160
+lat0 = 35
+subset(ds_duacs, ds_mdt, lon0, lat0, domain_name='kuroshio', duacs=True)
+subset(ds ,ds_mdt , lon0, lat0, domain_name='kuroshio', duacs=False)
