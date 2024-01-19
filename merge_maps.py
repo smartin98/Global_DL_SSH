7fea9aa866eacc8f644ca4e1da2ec318cdcaf943
smartin98 <s98amartin@gmail.com>
2024-01-19 09:48:26 -0800

    cleanup

--- merge_maps.py
+++ merge_maps.py
@@ -13,7 +13,7 @@ def worker(lock, batches):
                 break  # No more directories to process
 
             batch = batches.pop(0)  # Get the next directory
-        available_regions=np.array([i for i in range(5615)])#np.load('regions_south.npy')
+        available_regions=np.array([i for i in range(5615)])
         print(np.size(available_regions))
         merge_maps_and_save(pred_dir, pred_file_pattern, batch, output_nc_dir, mask_filename, dist_filename, mdt_filename, network_name, available_regions,L=250e3, crop_pixels=9, dx=7.5e3, with_grads=True, mask_coast_dist=0, lon_min=-180 ,lon_max=180, lat_min=-70, lat_max=80, res=1/10, progress=False)
         gc.collect()
@@ -33,8 +33,8 @@ if __name__ == '__main__':
     pred_dates = [datetime.date(2019,1,1)+datetime.timedelta(days=t) for t in range(365)] 
     output_nc_dir = './SimVP SSH-SST 1M 6sat grads/'
     os.system("mkdir "+output_nc_dir)
-    mask_filename = './land_water_mask_10grid.nc' # find in 2023a Ocean Data Challenge
-    dist_filename = './distance_to_nearest_coastlines_10grid.nc' # find in 2023a Ocean Data Challenge
+    mask_filename = './land_water_mask_10grid.nc' # find in Harvard Dataverse repo
+    dist_filename = './distance_to_nearest_coastlines_10grid.nc' # find in Harvard Dataverse repo
     mdt_filename = './mdt_hybrid_cnes_cls18_cmems2020_global.nc' # Chosen MDT, available from AVISO+/CMEMS
     network_name = 'SimVP_SSH_SST_1M_global'
     N_workers = 6 #number of cpus to parallelise across
